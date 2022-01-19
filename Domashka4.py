7b4a73fef38854dde58a8e808dea78a29a10311d
Serhii Kot <serhiikot.personal@outlook.com>
2022-01-19 10:41:34 +0200

    pravki

--- Domashka4.py
+++ Domashka4.py
@@ -1,5 +1,7 @@
 txt = "Скільки учнів в классі"
 k1, k2, k3 = int(input(f"{txt}, №1\n")), int(input(f"{txt}, №2\n")), int(input(f"{txt}, №3\n"))
-vsego_uchenikov = k1 + k2 + k3
-vsego_part = vsego_uchenikov // 2 + vsego_uchenikov % 2
+part1 = k1 // 2 + k1 % 2
+part2 = k2 // 2 + k2 % 2
+part3 = k3 // 2 + k3 % 2
+vsego_part = part1 + part2 + part3
 print(f'Потрібно купити {vsego_part} парт(и)!')
